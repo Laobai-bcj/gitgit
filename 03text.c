901ef50d275654b04890b76e5efed468ecaab96d
bai <bai@email.com>
2022-08-28 16:28:58 +0800

    master---03text.c-04text.c

---
+++ 03text.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+	int i = 0,x = 0,z = 0,sum = 0;
+	int arr[5];
+	for(i=0;i<5;i++){
+		printf("请输入第%d个数：",i);
+		scanf("%d",&x);
+		arr[i]=x;
+		if(arr[i]>0){
+			z++;
+			sum+=arr[i];
+		}
+	}
+	printf("正数有%d个,和为%d\n",z,sum);
+	return 0;
+}
